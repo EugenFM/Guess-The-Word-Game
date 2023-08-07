86e9d4667b71b5ca7ef74913fdc1c425ee32a98f
SFicara <sficara@edmundsgovtech.com>
2023-08-07 11:22:02 -0400

    Removed functions to start from scratch

--- js/script.js
+++ js/script.js
@@ -27,28 +27,3 @@ const playAgainButton = document.querySelector(".play-again");
 // Global varaiable for testing the game before fetchin data from API
 const word = "magnolia";
 
-// Function to add placeholders for each letter of the word until guessed (test word is magnolia)
-const placeholders = function (word) {
-    const placeholderLetters = [];
-    // An array for populating the circle symbols for each letter of the word (magnolia)
-    // A for..of method to loop through the characters of the word
-    for (const letter of word) {
-        // .push() method to add the circle symbols to the array placeholderLetters
-        placeholderLetters.push("●");
-        wordInProgress.innerText = placeholderLetters.join("");
-        // console.log(letter);
-    }
-    
-}
-placeholders(word);
-
-// a click event listener for the guess button
-guessButton.addEventListener("click", function (e) {
-    // to prevent the reloading behavior of the form
-    e.preventDefault();
-    const input = inputLetter.value;
-    console.log(input);
-    // to empty the value of the input after each guess
-    inputLetter.value = "";
-});
-
